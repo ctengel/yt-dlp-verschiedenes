27322cd7f8cb606cd59abb697e8c9cde8c5d215e
ctengel <50063864+ctengel@users.noreply.github.com>
2026-02-03 12:45:09 -0500

    very verbose until we get this working
    #2

--- yt_dlp_plugins/extractor/poculum.py
+++ yt_dlp_plugins/extractor/poculum.py
@@ -19,6 +19,7 @@ class TumblrBlogIE(InfoExtractor):
         """Extract a video entry from a post element"""
         slug = post.attrib['slug']
         pid = post.attrib['id']
+        self.to_screen(f'Considering post {pid} {slug}')
         video = post.find('video-player')
         if video is None:
             video = post.find('regular-body')
@@ -27,6 +28,7 @@ class TumblrBlogIE(InfoExtractor):
                 if re.match(r"^https?\:\/\/.+\.mp4$", link):
                     # TODO extract more metadata
                     # TODO make sure we are returning correct type for extraction
+                    self.to_screen(f"Found video {link}")
                     return {
                         "id": f"{slug}_{pid}",  # TODO let's think on this one
                         "title": slug,
@@ -35,16 +37,19 @@ class TumblrBlogIE(InfoExtractor):
                         "upload_date": None,
                     }
         # TODO fallback to tumblr IE
+        self.to_screen(f'novid')
         self.report_warning(f"NOVID {pid} {slug}")
         return None
 
     def _blog_entries(self, blog):
         url = f'http://{blog}.tumblr.com/api/read'
+        self.to_screen('API URL "%s" identified' % url)
         offset = 0
         #variables_common = self._make_variables(channel_name, *args)
         #entries_key = f'{self._ENTRY_KIND}s'
         for page_num in itertools.count():
             # TODO consider fatal=False
+            self.to_screen(f'Page of {page_num * self._PAGE_LIMIT} to {self._PAGE_LIMIT}')
             webpage = self._download_webpage(url,
                                              video_id=blog,
                                              note=f'Downloading Tumblr blog page {page_num+1}',
@@ -59,7 +64,7 @@ class TumblrBlogIE(InfoExtractor):
                     yield entry
           
     def _real_extract(self, url):
+        self.to_screen('URL "%s" being processed by poculum' % url)
         blog_1, blog_2 = self._match_valid_url(url).groups()
         blog = blog_2 or blog_1
-        url = f'http://{blog}.tumblr.com/api/read'
         return self.playlist_result(self._blog_entries(blog), playlist_id=blog, playlist_title=blog)
