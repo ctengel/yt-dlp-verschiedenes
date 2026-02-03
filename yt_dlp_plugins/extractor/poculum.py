a464953d3678b989bf1d960fe764daa308249a9d
ctengel <50063864+ctengel@users.noreply.github.com>
2026-02-03 17:23:22 -0500

    more work
    #2

--- yt_dlp_plugins/extractor/poculum.py
+++ yt_dlp_plugins/extractor/poculum.py
@@ -1,65 +1,78 @@
 # ⚠ Don't use relative imports
 import itertools
+import re
 from yt_dlp.extractor.common import InfoExtractor
-import xml.etree.ElementTree as ET
+from yt_dlp.utils import (
+    int_or_none,
+    url_or_none
+)
+
 
 class TumblrBlogIE(InfoExtractor):
     _WORKING = False
-    _VALID_URL = r'https?://(?P<blog_name_1>[^/?#&]+)\.tumblr\.com/(?:(?P<blog_name_2>[a-zA-Z\d-]+)/)?$'
+    _VALID_URL = r'https?://(?P<blog_name_1>[^/?#&]+)\.tumblr\.com/(?P<blog_name_2>[a-zA-Z\d-]+)?$'
+    _TESTS = []  # TODO build tests
     _PAGE_LIMIT = 50
 
-    @staticmethod
-    def _page2posts(page_xml):
-        """Get an array of posts from an XML string"""
-        root = ET.fromstring(page_xml)
-        postselm = root.find('posts')
-        return postselm.findall('post')
-
-    def _extract_entry(self, post):
+    def _extract_entry(self, post, blog):
         """Extract a video entry from a post element"""
-        slug = post.attrib['slug']
-        pid = post.attrib['id']
+
+        # extract post metadata
+        slug = post.attrib.get('slug')
+        pid = post.attrib.get('id')
+        uploader_id = post.attrib.get('reblogged-root-name', blog)
+        repost_count = int_or_none(post.attrib.get('notes'))
+        post_url = url_or_none(post.attrib.get('url')) # TODO is this correct format???
+        timestamp = int_or_none(post.attrib['unix-timestamp'])
+
+        # Look for a video URL
         self.to_screen(f'Considering post {pid} {slug}')
+        # TODO see if we can learn anything else from the main extractor, such as finding multiple/best format; it seems like the best one comes up first so we are OK)
         video = post.find('video-player')
         if video is None:
             video = post.find('regular-body')
         if video is not None:
             for link in video.text.split('"'):
                 if re.match(r"^https?\:\/\/.+\.mp4$", link):
-                    # TODO extract more metadata
-                    # TODO make sure we are returning correct type for extraction
                     self.to_screen(f"Found video {link}")
                     return {
-                        "id": f"{slug}_{pid}",  # TODO let's think on this one
-                        "title": slug,
+                        "id": f"{slug}_{pid}",  # TODO let's think on this one; alt would be post ID to match with main extractor
+                        "title": slug or blog,  # TODO try to use post title instead before resorting to slug
+                        "uploader_id": uploader_id,
+                        'uploader_url': f'https://{uploader_id}.tumblr.com/' if uploader_id else None,  # TODO is this correct format?
+                        'repost_count': repost_count,
                         "url": link,
+                        "display_id": slug or None,
                         "ext": "mp4",
-                        "upload_date": None,
+                        "is_live": False,
+                        "protocol": "https",
+                        "timestamp": timestamp
+                        # TODO "description" (video-caption or regular-body)?
+                        # TODO duration
+                        # TODO thumbnail
                     }
-        # TODO fallback to tumblr IE
-        self.to_screen(f'novid')
-        self.report_warning(f"NOVID {pid} {slug}")
-        return None
+        
+        # Nothing found, fallback
+        # NOTE in the unlikely event we have both native and non-native video, we will only get the native (vs a playlist with all)
+        self.report_warning(f"Could not find a video so falling back on main extractor for post {pid} {slug}")
+        return self.url_result(post_url, 'Tumblr', pid)
+
 
     def _blog_entries(self, blog):
         url = f'http://{blog}.tumblr.com/api/read'
         self.to_screen('API URL "%s" identified' % url)
-        offset = 0
-        #variables_common = self._make_variables(channel_name, *args)
-        #entries_key = f'{self._ENTRY_KIND}s'
         for page_num in itertools.count():
-            # TODO consider fatal=False
             self.to_screen(f'Page of {page_num * self._PAGE_LIMIT} to {self._PAGE_LIMIT}')
-            webpage = self._download_webpage(url,
-                                             video_id=blog,
-                                             note=f'Downloading Tumblr blog page {page_num+1}',
-                                             query={"start": page_num * self._PAGE_LIMIT, "num": self._PAGE_LIMIT})
-            posts = self._page2posts(webpage)
+            webpage = self._download_xml(url,
+                                         video_id=blog,
+                                         note=f'Downloading Tumblr blog page {page_num+1}',
+                                         query={"start": page_num * self._PAGE_LIMIT, "num": self._PAGE_LIMIT})
+            posts = webpage.find('posts').findall('post')
+            self.to_screen(f'Got {len(posts)} posts')
             if not posts:
                 break
             for post in posts:
-                # TODO consider possibility of multiple videos
-                entry = self._extract_entry(post)
+                entry = self._extract_entry(post, blog)
                 if entry:
                     yield entry
           
